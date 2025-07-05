af57d968eb3d90295072f4386aafa475d1deade2
codewithshashwat <pritishshashwat@gmail.com>
2025-07-05 11:44:52 +0530

    Created models for comments

--- iCoder/urls.py
+++ iCoder/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = "iCoder Admin"
+admin.site.site_title = "Admin Panel"
+admin.site.index_title = "Welcome onboard"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
