e2c2c79d89b0095f5ae7a6bee4e4a47559f2a7c6
codewithshashwat <pritishshashwat@gmail.com>
2025-07-05 11:19:24 +0530

    Created backend for auth

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about',views.about,name="About"),
     path('',views.home,name="Home"),
     path('search', views.search, name="search"),
+    path('signup', views.handleSignUp, name="handleSignUp"),
+    path('login', views.handeLogin, name="handleLogin"),
+    path('logout', views.handelLogout, name="handleLogout"),
 ]
